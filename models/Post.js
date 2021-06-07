a632235c277a40516b16a8eeeca294ee1b93f116
Swan Pham <accounting@nailsrus.ca>
2021-06-06 22:27:35 -0400

    update style

--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,7 @@ Post.init(
             allowNull: false
         },
         post_content: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,            
         },
         user_id: {
